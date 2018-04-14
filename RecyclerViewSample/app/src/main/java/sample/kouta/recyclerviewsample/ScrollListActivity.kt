b7b924b4212e1b88a27a7b1f0c28bc4d8e54456c
mattukouta <bb1016181@gmail.com>
2018-04-14 16:25:31 +0900

    リサイクラービュー

--- RecyclerViewSample/app/src/main/java/sample/kouta/recyclerviewsample/ScrollListActivity.kt
+++ RecyclerViewSample/app/src/main/java/sample/kouta/recyclerviewsample/ScrollListActivity.kt
@@ -4,6 +4,8 @@ import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.CollapsingToolbarLayout
+import android.support.v4.content.res.TypedArrayUtils.getString
+import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
@@ -11,6 +13,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import android.support.v7.widget.Toolbar
+import android.widget.Toast
 
 class ScrollListActivity : AppCompatActivity() {
 
@@ -32,6 +35,9 @@ class ScrollListActivity : AppCompatActivity() {
         var menuList=createTeisyokuList()
         var adapter=RecyclerListAdapter(menuList)
         lvMenu.adapter=adapter
+
+        var decorator=DividerItemDecoration(this@ScrollListActivity,layout.orientation)
+        lvMenu.addItemDecoration(decorator)
     }
 
     fun createTeisyokuList() : List<Map<String,Any>>{
@@ -40,67 +46,67 @@ class ScrollListActivity : AppCompatActivity() {
 
         menu.put("name","から揚げ定食")
         menu.put("price",800)
- //       menu.put("desc","若鶏のから揚げにサラダ、ご飯とお味噌汁が付きます。")
+        menu.put("desc","若鶏のから揚げにサラダ、ご飯とお味噌汁が付きます。")
         menuList.add(menu)
 
         menu= HashMap()
         menu.put("name","ハンバーグ定食")
-        menu.put("price",850)
-//        menu.put("desc","手ごねハンバーグにサラダ、ご飯とお味噌汁が付きます。")
+        menu["price"]=850
+        menu.put("desc","手ごねハンバーグにサラダ、ご飯とお味噌汁が付きます。")
         menuList.add(menu)
 
         menu=HashMap()
         menu.put("name","生姜焼き定食")
         menu.put("price",850)
-//        menu.put("desc","生姜焼きにサラダ、ご飯とお味噌汁が付きます。")
+        menu.put("desc","生姜焼きにサラダ、ご飯とお味噌汁が付きます。")
         menuList.add(menu)
 
         menu=HashMap()
         menu.put("name","ステーキ定食")
         menu.put("price",1000)
-//        menu.put("desc","ロースステーキにサラダ、ご飯とお味噌汁が付きます。")
+        menu.put("desc","ロースステーキにサラダ、ご飯とお味噌汁が付きます。")
         menuList.add(menu)
 
         menu=HashMap()
         menu.put("name","野菜炒め定食")
         menu.put("price",750)
-//        menu.put("desc","野菜炒めにサラダ、ご飯とお味噌汁が付きます。")
+        menu.put("desc","野菜炒めにサラダ、ご飯とお味噌汁が付きます。")
         menuList.add(menu)
 
         menu=HashMap()
         menu.put("name","とんかつ定食")
         menu.put("price",900)
-//        menu.put("desc","ロースとんかつにサラダ、ご飯とお味噌汁が付きます。")
+        menu.put("desc","ロースとんかつにサラダ、ご飯とお味噌汁が付きます。")
         menuList.add(menu)
 
         menu=HashMap()
         menu.put("name","ミンチかつ定食")
         menu.put("price",850)
-//        menu.put("desc","ミンチかつにサラダ、ご飯とお味噌汁が付きます。")
+        menu.put("desc","ミンチかつにサラダ、ご飯とお味噌汁が付きます。")
         menuList.add(menu)
 
         menu=HashMap()
         menu.put("name","チキンカツ定食")
         menu.put("price",900)
-//        menu.put("desc","チキンカツにサラダ、ご飯とお味噌汁が付きます。")
+        menu.put("desc","チキンカツにサラダ、ご飯とお味噌汁が付きます。")
         menuList.add(menu)
 
         menu=HashMap()
         menu.put("name","コロッケ定食")
         menu.put("price",850)
-//        menu.put("desc","手作りコロッケにサラダ、ご飯とお味噌汁が付きます。")
+        menu.put("desc","手作りコロッケにサラダ、ご飯とお味噌汁が付きます。")
         menuList.add(menu)
 
         menu=HashMap()
         menu.put("name","焼き魚定食")
         menu.put("price",750)
-//        menu.put("desc","旬の焼き魚にサラダ、ご飯とお味噌汁が付きます。")
+        menu.put("desc","旬の焼き魚にサラダ、ご飯とお味噌汁が付きます。")
         menuList.add(menu)
 
         menu=HashMap()
         menu.put("name","焼肉定食")
         menu.put("price",900)
-//        menu.put("desc","焼肉にサラダ、ご飯とお味噌汁が付きます。")
+        menu.put("desc","焼肉にサラダ、ご飯とお味噌汁が付きます。")
         menuList.add(menu)
 
         return menuList
@@ -119,6 +125,7 @@ class ScrollListActivity : AppCompatActivity() {
         override fun onCreateViewHolder(parent: ViewGroup,viewType:Int):RecyclerListViewHolder{
             var inflater=LayoutInflater.from(this@ScrollListActivity)
             var view=inflater.inflate(R.layout.row,parent,false)
+            view.setOnClickListener(ItemClickListener())
             return RecyclerListViewHolder(view)
         }
         override fun onBindViewHolder(holder: RecyclerListViewHolder, position: Int) {
@@ -131,6 +138,14 @@ class ScrollListActivity : AppCompatActivity() {
         }
         override fun getItemCount(): Int {
             return _listData.size
+        }
+    }
+    inner class ItemClickListener:View.OnClickListener{
+        override fun onClick(view: View?) {
+            var tvMenuName=view!!.findViewById<TextView>(R.id.tvMenuName)
+            var menuName=tvMenuName.text.toString()
+            var msg= getString(R.string.msg_header)+menuName
+            Toast.makeText(this@ScrollListActivity,msg,Toast.LENGTH_SHORT).show()
         }
     }
 }
